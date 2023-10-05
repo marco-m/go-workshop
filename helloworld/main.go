1cb57d5a823533ab66f792296ca2e089b4ea669c
Marco Molteni <marco.molteni@mailbox.org>
2023-10-05 15:51:34 +0200

    helloworld: show (silly) failure test

--- helloworld/main.go
+++ helloworld/main.go
@@ -8,13 +8,17 @@ import (
 )
 
 func main() {
-	if err := run(); err != nil {
+	if err := run(os.Args[1:]); err != nil {
 		fmt.Println("error:", err)
 		os.Exit(1)
 	}
 }
 
-func run() error {
+func run(cmdLine []string) error {
+	// This is silly; used only to show some more testing.
+	if len(cmdLine) > 0 {
+		return fmt.Errorf("unexpected: %q", cmdLine)
+	}
 	fmt.Println(hello.Greet("bob"))
 	return nil
 }
--- helloworld/main_test.go
+++ helloworld/main_test.go
@@ -6,8 +6,15 @@ import (
 	"github.com/go-quicktest/qt"
 )
 
+// Note that we are still not testing the output to stdout.
 func TestRunSuccess(t *testing.T) {
-	err := run()
+	err := run([]string{})
 
 	qt.Assert(t, qt.IsNil(err))
+}
+
+func TestRunFailure(t *testing.T) {
+	err := run([]string{"monkey"})
+
+	qt.Assert(t, qt.ErrorMatches(err, `unexpected: \["monkey"\]`))
 }
